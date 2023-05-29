e2d5b0e36752091363dfc2930d981a68c4a91da9
Анна Котляр <kotlyaranya1771@gmail.com>
2023-05-29 19:14:58 +0300

    -

---
+++ script.js
@@ -0,0 +1,20 @@
+// 1
+// alert( 'Я - JavaScript!' );
+
+// 2
+// 'use strict';
+// суворий режим
+
+// 3
+// let admin;
+// let name;
+// name = 'Іван';
+// admin = name;
+// alert(admin);
+
+// 4
+let planetEarth; // ourPlanetName
+let currentUser; // currentUserName
+
+// 5
+
